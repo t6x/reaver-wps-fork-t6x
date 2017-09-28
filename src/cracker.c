ce79753b99addfcdb5ffebdffa26db47e18ae99e
rofl0r <retnyg@gmx.net>
2017-09-28 16:25:08 +0100

    Revert "print confirmation that beacon was received"
    This reverts commit a543ff7bce87ac738788f2045ab4bca26189e8c0.
    
    the commit was done in a hurry, and the info displayed messes up stdout
    in some scenarios.

--- src/cracker.c
+++ src/cracker.c
@@ -74,9 +74,8 @@ void crack()
 		 * We need to get some basic info from the AP, and also want to make sure the target AP
 		 * actually exists, so wait for a beacon packet 
 		 */
-		cprintf(INFO, "[+] Waiting for beacon from %s...", bssid);
+		cprintf(INFO, "[+] Waiting for beacon from %s\n", bssid);
 		read_ap_beacon();
-		cprintf(INFO, "OK\n");
 	
 		/* I'm fairly certian there's a reason I put this in twice. Can't remember what it was now though... */	
 		if(get_max_pin_attempts() == -1)
