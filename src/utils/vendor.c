1f09f5e2eb76e9150cbe9c9e48531cb9d0472d20
kcdtv <kcdtv78@gmail.com>
2017-11-28 19:10:22 +0100

    Update vendor.c
    Add support in wash for Quantenna chipset detection

--- src/utils/vendor.c
+++ src/utils/vendor.c
@@ -19,6 +19,7 @@ char *get_vendor_string(const unsigned char* oui) {
 		{"\x00\x1c\x51", "CelenoCo"}, /* Celeno Communications */
 		{"\x00\x50\x43", "MarvellS"}, /* Marvell Semiconductor, Inc. */
 		{"\x00\x50\xf2", "Microsof"}  /* Microsoft */ 
+		{"\x00\x26\x86", "Quantenn"}  /* Microsoft */ 
 	};
 
 	#define VENDOR_LIST_SIZE (sizeof(vendors)/sizeof(vendors[0]))
