t6x/reaver-wps-fork-t6x

commit bdd420d7fe4114f6e5da46654b8c95cc22fd042b

MAC COMPAT: use pcap_next_ex and pcap_create for capture interface
based on the original patch 7c6c67b4451d33bef9c6b2d5ac6f00966a93bae3
from alex-chan.

without using this method, mac user get:
error: failed to compile packet filter

since this code works too on other platforms, there's no reason to put
it under ugly #ifdef APPLE's as the original patch did.

src/init.c
@@ -118,13 +118,29 @@ end:
 /* Initializes pcap capture settings and returns a pcap handle on success, NULL on error */
 pcap_t *capture_init(char *capture_source)
 {
-	pcap_t *handle = NULL;
+	pcap_t *handle;
 	char errbuf[PCAP_ERRBUF_SIZE] = { 0 };
-	
-	handle = pcap_open_live(capture_source, 65536, 1, 0, errbuf);
-	if(!handle)
-	{
-		handle = pcap_open_offline(capture_source, errbuf);
+
+	handle = pcap_open_offline(capture_source, errbuf);
+	if(handle) return handle;
+
+	handle = pcap_create(capture_source, errbuf);
+	if (handle) {
+		pcap_set_snaplen(handle, 65536);
+		pcap_set_timeout(handle, 50);
+		pcap_set_rfmon(handle, 1);
+		pcap_set_promisc(handle, 1);
+		int status = pcap_activate(handle);
+		if (status) {
+			cprintf(CRITICAL, "pcap_activate status %d\n", status);
+			pcap_close(handle);
+			handle = 0;
+		}
+	}
+
+	if(!handle) {
+		cprintf(CRITICAL, "couldn't get pcap handle, exiting\n");
+		exit(1);
 	}
 
 	return handle;

src/80211.c
@@ -37,11 +37,17 @@
 /*Reads the next packet from pcap_next() and validates the FCS. */
 u_char *next_packet(struct pcap_pkthdr *header)
 {
-	u_char *packet = NULL;
+	const u_char *packet = NULL;
+	struct pcap_pkthdr *pkt_header;
+	int status;
 
 	/* Loop until we get a valid packet, or until we run out of packets */
-	while((packet = (void*)pcap_next(get_handle(), header)) != NULL)
+	while((status = pcap_next_ex(get_handle(), &pkt_header, &packet)) == 1 || !status)
 	{
+		if(!status) continue; /* timeout */
+
+		memcpy(header, pkt_header, sizeof(*header));
+
 		if(get_validate_fcs())
 		{
 			if(check_fcs(packet, header->len))
@@ -59,7 +65,7 @@ u_char *next_packet(struct pcap_pkthdr *header)
 		}
 	}
 
-	return packet;
+	return (void*)packet;
 }
 
 /* 

commit 7d4153bb83edf7a5e0fe451bc5b270af35a45823

fix pcap_activate() regression
introduced in bdd420d7fe4114f6e5da46654b8c95cc22fd042b

some embedded linux devices are not compatible to rfmon setting.

also improve error messages.

src/init.c
@@ -120,6 +120,7 @@ pcap_t *capture_init(char *capture_source)
 {
 	pcap_t *handle;
 	char errbuf[PCAP_ERRBUF_SIZE] = { 0 };
+	int status;
 
 	handle = pcap_open_offline(capture_source, errbuf);
 	if(handle) return handle;
@@ -131,7 +132,6 @@ pcap_t *capture_init(char *capture_source)
 		char* argv[] = {"/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport", "-z", NULL};
 		execve("/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport", argv, NULL);
 	}
-	int status;
 	waitpid(pid,&status,0);
 #endif
 
@@ -141,12 +141,19 @@ pcap_t *capture_init(char *capture_source)
 		pcap_set_timeout(handle, 50);
 		pcap_set_rfmon(handle, 1);
 		pcap_set_promisc(handle, 1);
-		int status = pcap_activate(handle);
-		if (status) {
-			cprintf(CRITICAL, "pcap_activate status %d\n", status);
-			pcap_close(handle);
-			handle = 0;
+		if(!(status = pcap_activate(handle)))
+			return handle;
+		if(status == PCAP_ERROR_RFMON_NOTSUP) {
+			pcap_set_rfmon(handle, 0);
+			status = pcap_activate(handle);
+			if(!status) return handle;
 		}
+		cprintf(CRITICAL, "[X] ERROR: pcap_activate status %d\n", status);
+		if(status == PCAP_ERROR_NO_SUCH_DEVICE)
+			cprintf(CRITICAL, "[X] PCAP: no such device\n");
+		/* TODO : print nice error message for other codes */
+		pcap_close(handle);
+		handle = 0;
 	}
 
 	if(!handle) {

src/wpsmon.c
@@ -268,6 +268,7 @@ int main(int argc, char *argv[])
 		if(pcap_compile(get_handle(), &bpf, PACKET_FILTER, 0, 0) != 0)
 		{
 			cprintf(CRITICAL, "[X] ERROR: Failed to compile packet filter\n");
+			cprintf(CRITICAL, "[X] PCAP: %s\n", pcap_geterr(get_handle()));
 			goto end;
 		}
 		