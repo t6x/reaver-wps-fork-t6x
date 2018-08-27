7c619ebd8da749cac1729536dd3a2e23fe56463a
rofl0r <retnyg@gmx.net>
2018-08-27 05:35:13 +0100

    make pcap writer code endian-safe
    - also added support for RcB build tool

--- src/pcapfile.c
+++ src/pcapfile.c
@@ -1,10 +1,30 @@
+/* simple pcap file writer (C) 2018 rofl0r */
 #include <unistd.h>
 #include <pcap/pcap.h>
+#include "utils/endianness.h"
 
-void pcapfile_write_header(int outfd) {
+#ifdef SWITCH_ENDIAN
+/* if defined allows to use the opposite endian format for testing */
+#  define SWAP_IF_SWITCHED(X) end_bswap32(X)
+#  define NOT_IF_SWITCHED !
+#else
+#  define SWAP_IF_SWITCHED(X) X
+#  define NOT_IF_SWITCHED
+#endif
+
+static void pcapfile_write_header_be(int outfd) {
+	write(outfd, "\xA1\xB2\xC3\xD4" "\x00\x02\x00\x04"
+	             "\x00\x00\x00\x00" "\x00\x00\x00\x00"
+	             "\x00\x04\x00\x00" "\x00\x00\x00\x7F", 24);
+}
+static void pcapfile_write_header_le(int outfd) {
 	write(outfd, "\xD4\xC3\xB2\xA1" "\x02\x00\x04\x00"
 	             "\x00\x00\x00\x00" "\x00\x00\x00\x00"
 	             "\x00\x00\x04\x00" "\x7F\x00\x00\x00", 24);
+}
+void pcapfile_write_header(int outfd) {
+	if (NOT_IF_SWITCHED ENDIANNESS_BE) pcapfile_write_header_be(outfd);
+	else pcapfile_write_header_le(outfd);
 }
 
 void pcapfile_write_packet(int outfd, struct pcap_pkthdr *h_out, const unsigned char* data) {
@@ -14,10 +34,10 @@ void pcapfile_write_packet(int outfd, struct pcap_pkthdr *h_out, const unsigned 
 		unsigned caplen;
 		unsigned len;
 	} hdr_out = {
-		.sec_epoch = h_out->ts.tv_sec,
-		.ms_sec = h_out->ts.tv_usec,
-		.caplen = h_out->caplen,
-		.len = h_out->len,
+		.sec_epoch = SWAP_IF_SWITCHED(h_out->ts.tv_sec),
+		.ms_sec = SWAP_IF_SWITCHED(h_out->ts.tv_usec),
+		.caplen = SWAP_IF_SWITCHED(h_out->caplen),
+		.len = SWAP_IF_SWITCHED(h_out->len),
 	};
 	write(outfd, &hdr_out, sizeof hdr_out);
 	write(outfd, data, h_out->len);
--- src/pcapfile.h
+++ src/pcapfile.h
@@ -6,5 +6,7 @@
 void pcapfile_write_header(int outfd);
 void pcapfile_write_packet(int outfd, struct pcap_pkthdr *h_out, const unsigned char* data);
 
+//RcB: DEP "pcapfile.c"
+
 #endif
 
