188b6f4c2b82bddeef436b601780eb098b60eaec
Andreas Nilsen <adde88@gmail.com>
2021-02-15 01:31:14 +0100

    Add the -M, --mac-changer argument to Reaver. Default setting is to not enable it, if it isn't specified specifically.
    Signed-off-by: Andreas Nilsen <adde88@gmail.com>

--- src/argsparser.c
+++ src/argsparser.c
@@ -31,16 +31,7 @@
  *  files in the program, then also delete it here.
  */
 
-#include <stdio.h>
-#include <getopt.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include "globule.h"
-#include "defs.h"
-#include "iface.h"
 #include "argsparser.h"
-#include "pixie.h"
-#include "misc.h"
 
 /* Processes Reaver command line options */
 int process_arguments(int argc, char **argv)
@@ -50,9 +41,8 @@ int process_arguments(int argc, char **argv)
 	int long_opt_index = 0;
 	char bssid[MAC_ADDR_LEN] = { 0 };
 	char mac[MAC_ADDR_LEN] = { 0 };
-	char *short_options = "b:e:m:i:t:d:c:T:x:r:g:l:p:s:C:O:KZA5ELfnqvDShwN6JFu";
+	char *short_options = "b:e:m:i:t:d:c:T:x:r:g:l:o:p:s:C:aA5ELfnqvDShwNM";
 	struct option long_options[] = {
-		{ "pixie-dust", no_argument, NULL, 'K' },
 		{ "interface", required_argument, NULL, 'i' },
 		{ "bssid", required_argument, NULL, 'b' },
 		{ "essid", required_argument, NULL, 'e' },
@@ -66,6 +56,7 @@ int process_arguments(int argc, char **argv)
 		{ "session", required_argument, NULL, 's' },
 		{ "recurring-delay", required_argument, NULL, 'r' },
 		{ "max-attempts", required_argument, NULL, 'g' },
+		{ "out-file", required_argument, NULL, 'o' },
 		{ "pin", required_argument, NULL, 'p' },
 		{ "exec", required_argument, NULL, 'C' },
 		{ "no-associate", no_argument, NULL, 'A' },
@@ -73,21 +64,18 @@ int process_arguments(int argc, char **argv)
 		{ "no-nacks", no_argument, NULL, 'N' },
 		{ "eap-terminate", no_argument, NULL, 'E' },
 		{ "dh-small", no_argument, NULL, 'S' },
+		{ "auto", no_argument, NULL, 'a' },
 		{ "fixed", no_argument, NULL, 'f' },
+		{ "daemonize", no_argument, NULL, 'D' },
 		{ "5ghz", no_argument, NULL, '5' },
-		{ "repeat-m6", no_argument, NULL, '6' },
 		{ "nack", no_argument, NULL, 'n' },
 		{ "quiet", no_argument, NULL, 'q' },
 		{ "verbose", no_argument, NULL, 'v' },
 		{ "win7", no_argument, NULL, 'w' },
 		{ "help", no_argument, NULL, 'h' },
-		{ "timeout-is-nack", no_argument, NULL, 'J' },
-		{ "ignore-fcs", no_argument, NULL, 'F' },
-		{ "output-file", required_argument, NULL, 'O'},
+		{ "mac-changer", no_argument, NULL, 'M' },
 		{ 0, 0, 0, 0 }
 	};
-
-	set_output_fd(-1);
 
 	/* Since this function may be called multiple times, be sure to set opt index to 0 each time */
 	optind = 0;
@@ -96,33 +84,18 @@ int process_arguments(int argc, char **argv)
         {
                 switch(c)
                 {
-			case 'O':
-				{
-					int ofd = open(optarg, O_WRONLY|O_CREAT|O_TRUNC, 0660);
-					set_output_fd(ofd);
-					if(ofd == -1) perror("open outputfile failed: ");
-				}
-				break;
-                        case 'Z':
-                        case 'K':
-				pixie.do_pixie = 1;
-				set_max_pin_attempts(1);
-				break;
-			case 'u':
-				pixie.use_uptime = 1;
-				break;
                         case 'i':
                                 set_iface(optarg);
                                 break;
                         case 'b':
-                                str2mac(optarg, (unsigned char *) &bssid);
+                                str2mac((unsigned char *) optarg, (unsigned char *) &bssid);
                                 set_bssid((unsigned char *) &bssid);
                                 break;
                         case 'e':
                                 set_ssid(optarg);
                                 break;
                         case 'm':
-                                str2mac(optarg, (unsigned char *) &mac);
+                                str2mac((unsigned char *) optarg, (unsigned char *) &mac);
                                 set_mac((unsigned char *) &mac);
                                 break;
                         case 't':
@@ -138,9 +111,6 @@ int process_arguments(int argc, char **argv)
                         case '5':
                                 set_wifi_band(AN_BAND);
                                 break;
-                        case '6':
-                                set_repeat_m6(1);
-                                break;
                         case 'd':
                                 set_delay(atoi(optarg));
                                 break;
@@ -149,7 +119,6 @@ int process_arguments(int argc, char **argv)
                                 break;
 			case 'p':
 				parse_static_pin(optarg);
-				set_max_pin_attempts(1);
 				break;
 			case 's':       
 				set_session(optarg);   
@@ -163,6 +132,12 @@ int process_arguments(int argc, char **argv)
                         case 'L':
                                 set_ignore_locks(1);
                                 break;
+			case 'a':       
+				set_auto_detect_options(1); 
+				break;
+			case 'o':
+				set_log_file(fopen(optarg, "w"));
+				break;
                         case 'x':
                                 set_fail_delay(atoi(optarg));
                                 break;
@@ -172,6 +147,9 @@ int process_arguments(int argc, char **argv)
                         case 'g':
                                 set_max_pin_attempts(atoi(optarg));
                                 break;
+                        case 'D':
+				daemonize();
+				break;
 			case 'E':
                                 set_eap_terminate(1);
                                 break;
@@ -179,10 +157,7 @@ int process_arguments(int argc, char **argv)
 				set_dh_small(1);
 				break;
                         case 'n':
-				cprintf(INFO, "[+] ignoring obsolete -n switch\n");
-				break;
-			case 'J':
-                                set_timeout_is_nack(1);
+                                set_timeout_is_nack(0);
                                 break;
                         case 'f':
                                 set_fixed_channel(1);
@@ -199,8 +174,8 @@ int process_arguments(int argc, char **argv)
 			case 'N':
 				set_oo_send_nack(0);
 				break;
-			case 'F':
-				set_validate_fcs(0);
+			case 'M':
+				set_mac_changer(1);
 				break;
                         default:
                                 ret_val = EXIT_FAILURE;
@@ -224,12 +199,10 @@ void init_default_settings(void)
         set_lock_delay(DEFAULT_LOCK_DELAY);
         set_debug(INFO);
         set_auto_channel_select(1);
-        set_timeout_is_nack(0);
+        set_timeout_is_nack(1);
 	set_oo_send_nack(1);
         set_wifi_band(BG_BAND);
-	set_validate_fcs(1);
-	pixie.do_pixie = 0;
-	set_pin_string_mode(0);
+	set_mac_changer(0);
 }
 
 /* Parses the recurring delay optarg */
@@ -252,29 +225,6 @@ void parse_recurring_delay(char *arg)
         free(x);
 }
 
-int is_valid_pin(char *pin)
-{
-    if(!pin)
-        return 0;
-
-    int i;
-    for (i = 0; i < strlen(pin); i++)
-    {
-         if(!isdigit(pin[i]))
-             return 0;
-    }
-    if(strlen(pin) == 8)
-    {
-        char pin7[8] = { 0 };
-        char pin8[9] = { 0 };
-        memcpy((void *) &pin7, pin, sizeof(pin7)-1);
-        snprintf(pin8, 9, "%s%d", pin7, wps_pin_checksum(atoi(pin7)));
-        if (strcmp(pin, pin8) != 0)
-            return 0;
-    }
-    return 1;
-}
-
 /* Parse the WPS pin to use into p1 and p2 */
 void parse_static_pin(char *pin)
 {
@@ -282,23 +232,71 @@ void parse_static_pin(char *pin)
 	char p1[5] = { 0 };
 	char p2[4] = { 0 };
 
-	len = strlen(pin);
-
-	if((len == 4 || len == 7 || len == 8) && is_valid_pin(pin) != 0)
+	if(pin)
 	{
-		memcpy((void *) &p1, pin, sizeof(p1)-1);
-		set_static_p1((char *) &p1);
+		len = strlen(pin);
 
-		if(len > 4)
+		if(len == 4 || len == 7 || len == 8)
 		{
-			memcpy((void *) &p2, pin+sizeof(p1)-1, sizeof(p2)-1);
-			set_static_p2((char *) &p2);
+			memcpy((void *) &p1, pin, sizeof(p1)-1);
+			set_static_p1((char *) &p1);
+			set_key_status(KEY2_WIP);
+
+			if(len > 4)
+			{
+				memcpy((void *) &p2, pin+sizeof(p1)-1, sizeof(p2)-1);
+				set_static_p2((char *) &p2);
+			}
 		}
-	}
-	else
-	{
-		set_pin_string_mode(1);
-		set_static_p1(pin);
+		else
+		{
+			cprintf(CRITICAL, "[X] ERROR: Invalid pin specified! Ignoring '%s'.\n", pin);
+		}
 	}
 }
 
+/* Process auto-applied options from the database. read_ap_beacon should be called before this. */
+void process_auto_options(void)
+{
+	char **argv = NULL;
+	int argc = 0, i = 0;
+	char *bssid = NULL, *ssid = NULL;
+
+	if(get_auto_detect_options())
+	{
+		bssid = (char *) mac2str(get_bssid(), ':');
+
+
+		if(bssid)
+		{
+			/* If we didn't get the SSID from the beacon packet, check the database */
+			if(get_ssid() == NULL)
+			{
+				ssid = get_db_ssid(bssid);
+				if(ssid)
+				{
+					set_ssid(ssid);
+					free(ssid);
+				}
+			}
+
+			argv = auto_detect_settings(bssid, &argc);
+			if(argc > 1 && argv != NULL)
+			{
+				/* Process the command line arguments */
+				process_arguments(argc, argv);
+
+				/* Clean up argument memory allocation */
+				for(i=0; i<argc; i++)
+				{
+					free(argv[i]);
+				}
+				free(argv);
+			}
+
+			free(bssid);
+		}
+	}
+
+	return;
+}
